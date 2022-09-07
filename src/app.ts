34ae1f326d8fcc77e2237930b887b5d419d4663a
Nolan Flegel <nolanflegel@outlook.com>
2022-09-07 08:47:27 -0600

    hello world tutorial

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,7 @@
-let message: string = 'Hello, World!';
-console.log(message);
+let message: string = 'Hello, Typescript!';
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
+
